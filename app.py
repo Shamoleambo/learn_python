4021fbc166bee7af309ec5b1c5858b80ed546d2f
Tiago Gomes <tidgomes@gmail.com>
2025-05-26 17:40:23 -0300

    learn about append

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 favorite_numbers = [1, 2, 3, 4, 7, 11, 12, 14, 17, 18, 21, 22, 23, 24, 27]
 favorite_letters = ["a", "f", "h", "l", "m", "r", "s", "t", "v"]
 
-favorite_letters.extend(favorite_numbers)
+favorite_letters.append("─")
 print (favorite_numbers)
 print(favorite_letters)

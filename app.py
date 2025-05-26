e9e5d717403ae42aadd9b6e36c7b0c2b3e3d1f5f
Tiago Gomes <tidgomes@gmail.com>
2025-05-26 17:42:11 -0300

    learn about the remove and insert functions

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ favorite_numbers = [1, 2, 3, 4, 7, 11, 12, 14, 17, 18, 21, 22, 23, 24, 27]
 favorite_letters = ["a", "f", "h", "l", "m", "r", "s", "t", "v"]
 
 favorite_letters.append("─")
+favorite_numbers.insert(2, "Mano")
+favorite_letters.remove("h")
 print (favorite_numbers)
 print(favorite_letters)

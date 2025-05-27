819748c53be2d420e9ba7662f0cc1e8450bc3cc3
Tiago Gomes <tidgomes@gmail.com>
2025-05-27 17:37:19 -0300

    try except block

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-list_of_lists = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [10,11,12]
-]
 
-for list in list_of_lists:
-    print(list)
-    for element in list:
-        print(element)
+try:
+    num = int(input("Enter a number: "))
+    print(num)
+except:
+    print("Yo, yo, yo! You didn't enter a number... why tho?")

a98f5afaeded8144e966181cdf16376de6fc7fbe
Tiago Gomes <tidgomes@gmail.com>
2025-05-27 17:32:25 -0300

    lists of lists

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-list_of_numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+list_of_lists = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [10,11,12]
+]
 
-for number in list_of_numbers:
-    print(number)
-
-print("\nI am done with numbers\n\n")
-
-for letter in "Tiago":
-    print(letter)
-
-print("\nI am done with letters")
+for list in list_of_lists:
+    print(list)
+    for element in list:
+        print(element)

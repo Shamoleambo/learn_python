8374d54370d59a401015c83d6a5198b17733a394
Tiago Gomes <tidgomes@gmail.com>
2025-05-27 16:26:29 -0300

    learn not and elif

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 is_animal = True
-is_cat = False
+is_cat = True
 is_cute = True
 
-if is_animal and is_cat:
-    print("You are a cat, which is an animal by the way")
+if is_animal and not is_cat:
+    print("You are a animal, but not a cat. Not a cat indeed")
+elif is_animal and is_cat and is_cute:
+    print("You are a cute cat")
 else:
     print("You are not a cat neither an animal, what are you?")

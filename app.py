40b986630605dafc7c8c000b2b7c6dd71ed84350
Tiago Gomes <tidgomes@gmail.com>
2025-05-27 16:21:55 -0300

    learn ifs and elses

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-def square(num):
-    return num*num
+is_animal = True
+is_cat = False
+is_cute = True
 
-num = square(2)
-print(num)
+if is_animal and is_cat:
+    print("You are a cat, which is an animal by the way")
+else:
+    print("You are not a cat neither an animal, what are you?")

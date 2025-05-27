6f0be00c98487e35a4e6b0e2697aea2a916a249e
Tiago Gomes <tidgomes@gmail.com>
2025-05-27 16:46:44 -0300

    for loops are easy

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-i = 1
+list_of_numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-while i <= 12:
-    print(i)
-    i += 1
+for number in list_of_numbers:
+    print(number)
 
-print("\nI am done")
+print("\nI am done with numbers\n\n")
+
+for letter in "Tiago":
+    print(letter)
+
+print("\nI am done with letters")

4ccfe07862e1e115d7588c0edb58a2d2ed975737
Tiago Gomes <tidgomes@gmail.com>
2025-05-28 17:41:48 -0300

    start reading files with python

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
+cats_file = open("cats.txt", "r")
 
-try:
-    num = int(input("Enter a number: "))
-    print(num)
-except ValueError as err:
-    print("Yo, yo, yo! You didn't enter a number... why tho?")
-    print(err)
+print(cats_file.readable())
+
+cats_file.close()
